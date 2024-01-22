da6168f007d8c82e2ebd2138f2f53ba03a48e796
Vladimir <gordienko308@gmail.com>
2024-01-22 17:47:20 +0300

    123

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10">
       <div className={'grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4'}>
-        {data.products.map((product)=>
-          <ProductItem 
-          product={product}
-          key={product.slug}         
+        {data.products.map((product) =>
+          <ProductItem
+            product={product}
+            key={product.slug}
           />
         )}
       </div>
