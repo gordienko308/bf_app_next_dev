074309050e2f8422072eda8afbca2a4e316bf982
Vladimir <gordienko308@gmail.com>
2024-01-23 18:52:39 +0300

    sidenav

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
 import '@/app/ui/globals.css';
+import SideNav from '@/app/ui/sidenav';
+
+
 
 /*export const metadata: Metadata = {
   title: 'beautyfreedom',
@@ -11,9 +14,15 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={''}>{children}
-      </body>      
-    </html>
+    /*<html lang="en">*/
+     /* <body className={''}>{children}*/
+        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+          <div className="w-full flex-none md:w-64">
+            <SideNav />
+          </div>
+          <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+        </div>
+     /* </body>*/
+    /*</html>*/
   )
 }
