4043daeda9c2203a41cd78912d2c53b8609fef1f
Vladimir <gordienko308@gmail.com>
2024-01-23 19:05:13 +0300

    add catalog page

---
+++ app/catalog/page.tsx
@@ -0,0 +1,11 @@
+import styles from '@/app/ui/home.module.css';
+
+export default function Page() {
+    return (
+    <main>
+        <div>
+            <h1> catalog page </h1>
+            
+        </div>
+    </main>)
+}
