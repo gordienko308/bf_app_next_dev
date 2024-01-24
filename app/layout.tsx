ce08dd184839d3184055001d25b3664ab1d6a57e
Vladimir <gordienko308@gmail.com>
2024-01-24 10:22:32 +0300

    bug 5

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,10 +17,10 @@ export default function RootLayout({
     /*<html lang="en">*/
      /* <body className={''}>{children}*/
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-          <div className=''/*"w-full flex-none md:w-64"*/>
-            {/*<SideNav />*/}
+          <div className="w-full flex-none md:w-64">
+            <SideNav />
           </div>
-          <div className=''/*"flex-grow p-2 md:overflow-y-auto md:p-12"*/>{children}</div>
+          <div className="flex-grow p-2 md:overflow-y-auto md:p-12">{children}</div>
         </div>
      /* </body>*/
     /*</html>*/
