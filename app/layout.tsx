2de91133071ca25940ca8418ffd4d1e996dfd373
Vladimir <gordienko308@gmail.com>
2024-01-25 16:31:08 +0300

    asfdsf

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+    
     <html lang="en">
      {/* <body className={''}>{children}*/}
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
